2089e9c3200b7cfd5facd5d0d1c4d89e59c27efe
Hokket <mateus_fer@hotmail.com>
2026-05-10 16:58:21 -0300

    feat: improve font selection and add system font retrieval

--- src/pdf_app/ui/text_toolbar.py
+++ src/pdf_app/ui/text_toolbar.py
@@ -1,6 +1,8 @@
+from gi.repository import Gtk, Gdk, GLib, Pango, PangoCairo
 import gi
 gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk, Gdk, GLib
+gi.require_version('Pango', '1.0')
+
 
 class TextFormattingToolbar(Gtk.Popover):
     def __init__(self, parent_widget, annotation, store, on_update):
@@ -9,17 +11,18 @@ class TextFormattingToolbar(Gtk.Popover):
         self.annotation = annotation
         self.store = store
         self.on_update = on_update
-        self.parent_widget = parent_widget 
-        
+        self.parent_widget = parent_widget
+
         box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=4)
         box.set_margin_top(4)
         box.set_margin_bottom(4)
         box.set_margin_start(4)
         box.set_margin_end(4)
         self.set_child(box)
-        
+
         # Font Family
-        self.font_model = Gtk.StringList.new(["Comfortaa", "cursive", "Cantarell", "Sans", "Serif", "Monospace"])
+        font_list = self._get_system_fonts()
+        self.font_model = Gtk.StringList.new(font_list)
         self.font_dropdown = Gtk.DropDown.new(self.font_model, None)
         self._select_font(self.annotation.font_family)
         self.font_dropdown.connect("notify::selected", self.on_font_changed)
@@ -38,7 +41,6 @@ class TextFormattingToolbar(Gtk.Popover):
         self.color_button = Gtk.ColorDialogButton()
         color_dialog = Gtk.ColorDialog()
         self.color_button.set_dialog(color_dialog)
-        
         rgba = Gdk.RGBA()
         r, g, b, a = self.annotation.color
         rgba.red, rgba.green, rgba.blue, rgba.alpha = r, g, b, 1.0
@@ -55,14 +57,17 @@ class TextFormattingToolbar(Gtk.Popover):
         self.bold_btn.connect("toggled", self.on_style_toggled, 'bold')
         box.append(self.bold_btn)
 
-        self.italic_btn = Gtk.ToggleButton(icon_name="format-text-italic-symbolic")
+        self.italic_btn = Gtk.ToggleButton(
+            icon_name="format-text-italic-symbolic")
         self.italic_btn.set_active(self.annotation.italic)
         self.italic_btn.connect("toggled", self.on_style_toggled, 'italic')
         box.append(self.italic_btn)
 
-        self.underline_btn = Gtk.ToggleButton(icon_name="format-text-underline-symbolic")
+        self.underline_btn = Gtk.ToggleButton(
+            icon_name="format-text-underline-symbolic")
         self.underline_btn.set_active(self.annotation.underline)
-        self.underline_btn.connect("toggled", self.on_style_toggled, 'underline')
+        self.underline_btn.connect(
+            "toggled", self.on_style_toggled, 'underline')
         box.append(self.underline_btn)
 
         # Separator
@@ -73,9 +78,24 @@ class TextFormattingToolbar(Gtk.Popover):
         self.delete_btn.add_css_class("destructive-action")
         self.delete_btn.connect("clicked", self.on_delete_clicked)
         box.append(self.delete_btn)
-        
         self.set_position(Gtk.PositionType.TOP)
         self.set_autohide(False)
+
+    def _get_system_fonts(self):
+        """Gets a list of all available fonts on the system"""
+        try:
+            font_map = PangoCairo.font_map_get_default()
+            families = font_map.list_families()
+            system_font_names = {f.get_name()
+                                 for f in families if f.get_name()}
+            fallback_fonts = {"Comfortaa", "cursive",
+                              "Cantarell", "Sans", "Serif", "Monospace"}
+            all_font_names = sorted(
+                list(system_font_names.union(fallback_fonts)))
+            return all_font_names if all_font_names else ["Sans"]
+        except Exception as e:
+            # Pango's default generic fallbacks
+            return ["Comfortaa", "cursive", "Cantarell", "Sans", "Serif", "Monospace"]
 
     def _select_font(self, font_name):
         for i in range(self.font_model.get_n_items()):
@@ -84,21 +104,19 @@ class TextFormattingToolbar(Gtk.Popover):
                 return
         # Default to 0 if not found
         self.font_dropdown.set_selected(0)
-        
+
     def update_position(self, scale):
         if not self.annotation.rects:
             return
-            
         x, y, w, h = self.annotation.rects[0]
-        
         rect = Gdk.Rectangle()
         rect.x = int(x * scale)
         rect.y = int(y * scale)
         rect.width = int(w * scale)
-    
-        if rect.width < 10: rect.width = 10
-        if rect.height < 10: rect.height = 10
-            
+        if rect.width < 10:
+            rect.width = 10
+        if rect.height < 10:
+            rect.height = 10
         self.set_pointing_to(rect)
 
     def _record_style(self):
@@ -131,7 +149,8 @@ class TextFormattingToolbar(Gtk.Popover):
         rgba = button.get_rgba()
         new_color = (rgba.red, rgba.green, rgba.blue, 1.0)
         if new_color != self.annotation.color:
-            self.store.record_color_change(self.annotation.id, self.annotation.color)
+            self.store.record_color_change(
+                self.annotation.id, self.annotation.color)
             self.annotation.color = new_color
             self._notify_update()
 
