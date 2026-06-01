b12458a4b6da2e13eca64104d45b5f6a54c3c323
ikabrain <ikanshmahajan@gmail.com>
2026-06-01 20:57:24 +0530

    fix(loading): log Poppler open failure before discarding GLib.Error

--- src/inlinea/document/loading.py
+++ src/inlinea/document/loading.py
@@ -1,6 +1,9 @@
+import logging
 import gi
 gi.require_version('Poppler', '0.18')
 from gi.repository import Poppler, Gio, GLib
+
+logger = logging.getLogger(__name__)
 
 def load_document(file: Gio.File, password: str = None) -> Poppler.Document:
     uri = file.get_uri()
@@ -8,4 +11,5 @@ def load_document(file: Gio.File, password: str = None) -> Poppler.Document:
         document = Poppler.Document.new_from_file(uri, password)
         return document
     except GLib.Error:
+        logger.warning("Could not open %s via Poppler", uri, exc_info=True)
         return None
